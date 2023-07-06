4e2ab5e7b2210ffafc9e371b8122c00910dfc7ff
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-06 20:48:36 +0600

    connect component with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './redux/store';
 function App() {
+  const {count} = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch()
 
   return (
-    <h1>Tech Net</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex gap-6 items-center">
+      <button className="border-2 border-green-500 rounded-md px-2 py-3">
+        Increment
+      </button>
+      <div>{count}</div>
+      <button className="border-2 border-red-500 rounded-md px-2 py-3">
+        Decrement
+      </button>
+      </div>
+    </div>
   )
 }
 
