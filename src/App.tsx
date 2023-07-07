f9d725e5d6bcd4a0b2a3cf76bb758627523f3ad0
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-07 23:19:41 +0600

    frontend design

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { decrement, increment, incrementByAmount } from './redux/features/counter/counterSlice';
-import { useAppDispatch, useAppSelector } from './redux/hook';
-import { RootState } from './redux/store';
+import { Toaster } from './components/ui/Toaster';
+import MainLayout from './layout/MainLayout';
 
 function App() {
-  const {count} = useAppSelector((state: RootState) => state.counter);
-  const dispatch = useAppDispatch()
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex gap-6 items-center">
-      <button className="border-2 border-green-500 rounded-md px-2 py-3"
-      onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <button className="border-2 border-green-500 rounded-md px-2 py-3"
-      onClick={() => dispatch(incrementByAmount(5))}>
-        Increment by 5
-      </button>
-      <div>{count}</div>
-      <button className="border-2 border-red-500 rounded-md px-2 py-3"
-      onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
-      </div>
+    <div>
+      <Toaster />
+      <MainLayout />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import store from './redux/store'
-import routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import routes from './routes/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <RouterProvider router={routes}/>
-    </Provider>
+    <RouterProvider router={routes} />
   </React.StrictMode>
-)
+);
