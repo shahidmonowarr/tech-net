717d977c08f243b12a8d9bb20f8b48314229ec3b
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-09 11:50:00 +0600

    [Done] Total count and checkout page

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -4,10 +4,12 @@ import { IProduct } from "../../../types/globalTypes";
 
 interface ICart {
   products: IProduct[];
+  total: number;
 }
 
 const initialState: ICart = {
   products: [],
+  total: 0,
 };
 
 const cartSlice = createSlice({
@@ -24,6 +26,8 @@ const cartSlice = createSlice({
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+
+      state.total += action.payload.price!;
     },
     removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
       const existing = state.products.find(
@@ -37,11 +41,15 @@ const cartSlice = createSlice({
           (product) => product._id !== action.payload._id
         );
       }
+
+      state.total -= action.payload.price!;
     },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
       );
+
+      state.total -= action.payload.price! * action.payload.quantity!;
     },
   },
 });
