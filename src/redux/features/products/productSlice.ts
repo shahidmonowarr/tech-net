f2161f0cea2d24e988f513a0f0c62d4e837e688e
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-09 12:17:18 +0600

    [Done] Product filter(priceRange/stock) Using redux

---
+++ src/redux/features/products/productSlice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface IProductFilter {
+  status: boolean;
+  priceRange: number;
+}
+
+const initialState: IProductFilter = {
+  status: false,
+  priceRange: 150,
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    toggleState: (state) => {
+      state.status = !state.status;
+    },
+    setPriceRange: (state, action: PayloadAction<number>) => {
+      state.priceRange = action.payload;
+    },
+  },
+});
+
+export const { toggleState, setPriceRange } = productSlice.actions;
+
+export default productSlice.reducer;
